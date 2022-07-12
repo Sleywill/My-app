aae4e3d0fca128f1707323c576b8b1a36706dbfb
Sleywil <whoisxjohngalt@gmail.com>
2022-07-12 20:47:42 +0300

    Initial Commit

---
+++ My app/My_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_appApp.swift
+//  My app
+//
+//  Created by Алексей Серебряков on 12.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct My_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
